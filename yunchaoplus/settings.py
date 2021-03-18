7202cdee195510ad06a14e79bd4f577b2522dcd7
Hcreak <hcreak@outlook.com>
2021-03-18 16:45:58 +0800

    Update Unify Address

--- yunchaoplus/settings.py
+++ yunchaoplus/settings.py
@@ -3,9 +3,11 @@
 # desc: 云钞plus 配置文件
 # date: 2021-02-19
 
-CHARGE_DNS = 'payment-t-yunchao.curdata.cn' #支付服务域名
-REFUND_DNS = 'refund-t-yunchao.curdata.cn'  #退款服务域名
-WALLET_DNS = 'wallet-t-yunchao.curdata.cn'  #钱包服务域名
-WITHDRAW_DNS = 'withdraw-t-yunchao.curdata.cn'  #提现服务域名
-TRANSFER_DNS = 'transfer-t-yunchao.curdata.cn'  #转账服务域名
-AGENTS_DNS = 'agents-t-yunchao.curdata.cn'  #签约服务域名
+Unify_Address = "t-yunchao.curdata.cn"
+
+CHARGE_DNS = Unify_Address #支付服务域名
+REFUND_DNS = Unify_Address  #退款服务域名
+WALLET_DNS = Unify_Address  #钱包服务域名
+WITHDRAW_DNS = Unify_Address  #提现服务域名
+TRANSFER_DNS = Unify_Address  #转账服务域名
+AGENTS_DNS = Unify_Address  #签约服务域名
